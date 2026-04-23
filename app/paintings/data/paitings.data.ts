4930eb6ef5504ab7bc5ab414a6372482f17ca31f
Goldfinch <facu_vila@hotmail.com>
2026-04-23 15:11:23 -0300

    se agregaron las pinturas de los cursos y los alumnos

--- app/paintings/data/paitings.data.ts
+++ app/paintings/data/paitings.data.ts
@@ -1,16 +1,54 @@
 import { Painting } from "../types/paitings.types";
 
 export const PAINTINGS: Painting[] = [
-  { src: "/students_paints/María_Del_Rosario.webp", author: "María Del Rosario", course: "Pintura Decorativa" },
-  { src: "/students_paints/Ayeza.webp",             author: "Ayeza",             course: "Dibujo y Pintura" },
-  { src: "/students_paints/Agostina.webp",          author: "Agostina",          course: "Acuarelas" },
-  { src: "/students_paints/Carmen.webp",            author: "Carmen",            course: "Pintura Decorativa" },
-  { src: "/students_paints/Edith.webp",             author: "Edith",             course: "Mix Media" },
-  { src: "/students_paints/Celeste.webp",           author: "Celeste",           course: "Paisajismo" },
-  { src: "/students_paints/Fabiana.webp",           author: "Fabiana",           course: "Pintura Country" },
-  { src: "/students_paints/Cecilia_K.webp",         author: "Cecilia K.",        course: "Acuarelas" },
+  {
+    src: "/students_paints/Cristina_deco.webp",
+    author: "Cristina",
+    course: "Pintura Decorativa",
+  },
+  {
+    src: "/students_paints/German_dibujo.webp",
+    author: "German",
+    course: "Dibujo y Pintura",
+  },
+  {
+    src: "/students_paints/Roxana_dibujo.webp",
+    author: "Roxana",
+    course: "Dibujo y Pintura",
+  },
+  {
+    src: "/students_paints/Dorita_mix.webp",
+    author: "Dorita",
+    course: "Mix Media",
+  },
+  { src: "/students_paints/lia_mix.webp", author: "Lia", course: "Mix Media" },
+  {
+    src: "/students_paints/Osky_paisajismo.webp",
+    author: "Oscar",
+    course: "Paisajismo",
+  },
+  {
+    src: "/students_paints/Vanesa_deco.webp",
+    author: "Vanesa",
+    course: "Pintura Decorativa",
+  },
+  {
+    src: "/students_paints/Rosario_dibujo.webp",
+    author: "Rosario",
+    course: "Dibujo y Pintura",
+  },
+  {
+    src: "/students_paints/Paula_mix.webp",
+    author: "Paula",
+    course: "Mix Media",
+  },
+  {
+    src: "/students_paints/Marisa_paisajismo.webp",
+    author: "Marisa",
+    course: "Paisajismo",
+  }
 ];
 
-export const CARD_WIDTH  = 300;
-export const CARD_GAP    = 18;
+export const CARD_WIDTH = 300;
+export const CARD_GAP = 18;
 export const CARD_HEIGHT = 400;
